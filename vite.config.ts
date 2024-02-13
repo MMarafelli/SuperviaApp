22b814714968e58a129b9187b2ae4bd54ac7dc4d
MMarafelli <mmarafelli@outlook.com>
2024-02-13 20:54:31 -0300

    Icone e instalação do PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+// you can copy the base structure of manifest object.
+const manifestForPlugIn : Partial<VitePWAOptions> = {
+  registerType:'prompt',
+  includeAssets:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"SuperViaApp",
+    short_name:"SuperViaApp",
+    description:"A SuperViaApp.",
+    theme_color:'#171717',
+    background_color:'#f0e7db',
+    display:"standalone",
+    scope:'/',
+    start_url:"/",
+    orientation:'portrait',
+    icons:[{
+      src: 'public/icons/sv_192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'public/icons/favicon.png',
+      sizes:'52x52',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ]
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 })
