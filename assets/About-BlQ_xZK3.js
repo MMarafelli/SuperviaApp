9be7b0c77d196b8b3a93f84aef7d68c9c09316ea
MMarafelli <MMarafelli@users.noreply.github.com>
2024-02-15 17:24:38 +0000

    deploy: a5c66b3f6b57cb0b0edcbc69eb884a3f8c96fb0c

---
+++ assets/About-BlQ_xZK3.js
@@ -0,0 +1,1 @@
+import{u as o,j as t}from"./index-BRSDCwhH.js";async function r(){return await new Promise(e=>setTimeout(e,500)),"I came from the About.tsx loader function!"}function a(){let e=o();return t.jsxs("div",{children:[t.jsx("h2",{children:"About"}),t.jsx("p",{children:e})]})}a.displayName="AboutPage";export{a as Component,r as loader};
