0ca70a673c967c88ff5a74d895484b1a29c1d9da
MMarafelli <MMarafelli@users.noreply.github.com>
2024-02-16 01:59:45 +0000

    deploy: 5d7926ae9f5fb0a3a97ceb828bcf81e11c4d842c

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/SuperViaApp/sw.js', { scope: '/SuperViaApp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
