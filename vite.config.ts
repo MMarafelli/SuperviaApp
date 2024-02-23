0c23004898f1aaa13caac347d6f4324d44e2079a
MMarafelli <mmarafelli@outlook.com>
2024-02-22 23:19:37 -0300

    Ajustes icones

--- vite.config.ts
+++ vite.config.ts
@@ -3,44 +3,33 @@ import react from '@vitejs/plugin-react'
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 // you can copy the base structure of manifest object.
-const manifestForPlugIn : Partial<VitePWAOptions> = {
-  registerType:'prompt',
-  includeAssets:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
-  manifest:{
-    name:"Super Via App",
-    short_name:"Super Via App",
-    description:"A Super Via App.",
-    theme_color:'#171717',
-    background_color:'#f0e7db',
-    display:"standalone",
+const manifestForPlugIn: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Super Via App",
+    short_name: "Super Via App",
+    description: "A Super Via App.",
+    theme_color: '#171717',
+    background_color: '#f0e7db',
+    display: "standalone",
     scope: "/SuperViaApp/",
     start_url: "/SuperViaApp/",
-    orientation:'portrait',
-    icons:[{
-      src: 'public/icons/sv_192x192.png',
-      sizes:'192x192',
-      type:'image/png',
-      purpose:'favicon'
-    },
-    {
-      src:'public/icons/favicon.png',
-      sizes:'52x52',
-      type:'image/png',
-      purpose:'favicon'
-    },
-    {
-      src: '/apple-touch-icon.png',
-      sizes:'180x180',
-      type:'image/png',
-      purpose:'apple touch icon',
-    },
-    {
-      src: '/maskable_icon.png',
-      sizes:'512x512',
-      type:'image/png',
-      purpose:'any maskable',
-    }
-  ]
+    orientation: 'portrait',
+    icons: [
+      {
+        src: 'public/icons/favicon.png',
+        sizes: '52x52',
+        type: 'image/png',
+        purpose: 'favicon'
+      },
+      {
+        src: 'public/icons/apple-touch-icon.png',
+        sizes: '52x52',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      }
+    ]
   }
 }
 
