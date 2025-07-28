46551b70f25991ed7dd38796f373f9d077d8fb38
MMarafelli <mmarafelli@outlook.com>
2025-07-27 23:08:29 -0300

    chore: limpeza códigos legados e backups

--- src/components/PageTitle.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/shared.css';
-
-interface PageTitleProps {
-    title: string;
-}
-
-export const PageTitle = ({ title }: PageTitleProps) => {
-    return <h1 className="page-title">{title}</h1>;
-};
